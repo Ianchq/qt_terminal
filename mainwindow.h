1012e8797952b97147f69efb20749a1485d36e83
Ianchq <iianik489@gmail.com>
2025-05-15 15:49:19 +0300

    Activity Diagrams

--- mainwindow.h
+++ mainwindow.h
@@ -33,14 +33,14 @@ private:
     QProcess *currentProcess = nullptr;
     void executeCommand(const QString &command);
     std::unordered_map<QString, std::function<void(const QString &)>> commandMap;
-    void runLsCommand(const QString &);
-    void runPwdCommand(const QString &);
-    void runEchoCommand(const QString &);
-    void printUnknownCommand(const QString &);
+    // void runLsCommand(const QString &);
+    // void runPwdCommand(const QString &);
+    // void runEchoCommand(const QString &);
+    // void printUnknownCommand(const QString &);
     void runCdCommand(const QStringList &arguments);
     void runBackgroundProcess(const QString &command);
     void handleCommand(const QString &input);
-    void runClearCommand(const QString &);
+    // void runClearCommand(const QString &);
 
 };
 
